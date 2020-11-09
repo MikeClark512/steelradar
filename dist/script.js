fd5750a5040cf24d86f643431e964f86f711570d
Mike Clark <mievlecl@gmail.com>
2020-11-09 10:43:55 -0700

    cleanup

--- dist/script.js
+++ dist/script.js
@@ -143,7 +143,30 @@ async function main() {
         .attr("fill", "#737373")
         .text(function (d) { return d; })
         ;
-
 }
 
 main();
+
+
+/*
+async function fetchText(url) { 
+    let response = await fetch(url);
+    if (response.status < 200 || response.status > 200) {
+        throw Error("fetchText status=" + response.status + " url=" + url);
+    }
+    let text = await response.text();
+    return text;
+}
+
+async function fetchJson(url) { 
+    let text = await fetchText(url);
+    return JSON.parse(text);
+}
+ 
+async function main() {
+    let data = await fetchJson("./data.json");
+    console.log("json", data);
+}
+
+main();
+*/
